ea0f6648c9b45e05d49610c0a78bc6e51f0757b1
SirHe <61593477+SirHe@users.noreply.github.com>
2022-05-23 00:39:15 +0800

    xx

--- auto.js
+++ auto.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const shell = require('shelljs')
 
-app.get('/restart', function (req, res) {
+app.get('/restart', (req, res) => {
   res.send('更新通知已收到，开始更新')
 
-  // 开放脚本权限
-  shell.exec('chmod u+x ./auto.sh')
-  // 执行脚本
-  shell.exec('./auto.sh')
+  // // 开放脚本权限
+  // shell.exec('chmod u+x ./auto.sh')
+  // // 执行脚本
+  // shell.exec('./auto.sh')
+  shell.exec('git pull')
+  shell.exec('yarn')
+  shell.exec('forever stopall')
+  shell.exec('forever start ./bin/www')
+  // shell.exit(1)
 })
 
-app.listen(5002, () => console.log('OK'))
+app.listen(3000, () => console.log('OK'))
--- auto.sh
+++
@@ -1,5 +0,0 @@
-# git reset --hard
-# git pull
-# yarn
-# kill -2 `lsof -i:8888`
-yarn start
